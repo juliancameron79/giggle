35efe38f4d33d6c9e63b35e593f87840af02f732
juliancameron79 <juliancameron79@gmail.com>
2021-08-08 14:55:40 +0200

    setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <Header></Header>
+    // <AddContact></AddContact>
+    // <ContactList></ContactList>
+  );
+}
+
+export default App;
